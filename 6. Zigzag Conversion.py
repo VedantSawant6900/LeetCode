9cafef52b65f92cb27c0051863cb6f722a1ac7d4
Vedant Sawant <vedant.sawant.6900@gmail.com>
2024-06-29 21:27:39 +0530

    dded 6. Zigzag Conversion

--- 6. Zigzag Conversion.py
+++ 6. Zigzag Conversion.py
@@ -2,23 +2,16 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        ans=[]
-        pivot_value = (numRows-1)*2
-        addition = [pivot_value,0]
-        for i in range(numRows):
-            ans.append("")
-            if i==0 or i == numRows-1:
-                for z in range(i,len(s),pivot_value):
-                    ans[i]=ans[i]+s[z]
-            else:
-                z=i
-                while z < len(s):
-                    ans[i]+=s[z]
-                    z=z+addition[0]
-                    if z >= len(s):
-                        break
-                    ans[i]+=s[z]
-                    z=z+addition[1]
-            addition[0]=addition[0]-2
-            addition[1]=addition[1]+2
+        ans=["" for _ in range(numRows)]
+        pointer=0
+        direction=1
+        for i in s:
+            ans[pointer]=ans[pointer]+i
+            pointer+=direction
+            if pointer<0:
+                pointer = 1
+                direction = 1
+            elif pointer==numRows:
+                pointer=pointer-2
+                direction = -1
         return "".join(ans)
